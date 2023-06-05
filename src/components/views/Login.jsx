c5121edd584ce1ff805b720245370e81b5de5bff
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-06-05 17:49:57 -0300

    agregamos el archivo de queries para hacer una consulta a json-server con la función de login

--- src/components/views/Login.jsx
+++ src/components/views/Login.jsx
@@ -1,32 +1,23 @@
-
-import { Form, Button, Container, Card } from "react-bootstrap";
-
+import { Form, Button, Container, Card } from 'react-bootstrap';
+import { login } from '../helpers/queries';
 
 const Login = () => {
+  login();
+
   return (
     <Container className="mainSection">
       <Card className="my-5">
         <Card.Header as="h5">Login</Card.Header>
         <Card.Body>
-          <Form >
+          <Form>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Ingrese un email"
-               
-              />
-             
+              <Form.Control type="email" placeholder="Ingrese un email" />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Password"
-               
-              />
-            
+              <Form.Control type="password" placeholder="Password" />
             </Form.Group>
             <Button variant="primary" type="submit">
               Ingresar
@@ -39,5 +30,3 @@ const Login = () => {
 };
 
 export default Login;
-
-
