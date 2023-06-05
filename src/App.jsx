25647811a471e8e242def6aecf929e799c8f1877
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-06-05 17:09:53 -0300

    agregamos las rutas y sus componentes

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,33 @@ import CrearProducto from './components/views/producto/CrearProducto';
 import EditarProducto from './components/views/producto/EditarProducto';
 import Administrador from './components/views/Administrador';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './components/views/Login';
+import Registro from './components/views/Registro';
 
 function App() {
   return (
     <BrowserRouter>
+      <Menu />
       <Routes>
         <Route exact path="/" element={<Inicio />} />
+        <Route exact path="/registro" element={<Registro />} />
+        <Route exact path="/login" element={<Login />}></Route>
+        <Route exact path="/detalle" element={<DetalleProducto />}></Route>
+        <Route exact path="/administrador" element={<Administrador />}></Route>
+        <Route
+          exact
+          path="/administrador/crear-producto"
+          element={<CrearProducto />}
+        ></Route>
+        <Route
+          exact
+          path="/administrador/editar-producto"
+          element={<EditarProducto />}
+        ></Route>
+        <Route path="*" element={<Error404 />}></Route>
       </Routes>
+      <Footer />
     </BrowserRouter>
-    // <>
-    //   <Menu></Menu>
-    //   {/* <Error404></Error404> */}
-    //   {/* <Inicio></Inicio> */}
-    //   {/* <DetalleProducto></DetalleProducto> */}
-    //   {/* <CrearProducto></CrearProducto> */}
-    //   {/* <EditarProducto></EditarProducto> */}
-    //   <Administrador></Administrador>
-    //   <Footer></Footer>
-    // </>
   );
 }
 
