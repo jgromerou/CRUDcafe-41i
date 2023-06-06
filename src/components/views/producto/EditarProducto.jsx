096e2ec73d39844dbb69f9083b9e873e24fe21e4
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-06-06 09:06:13 -0300

    se agregó las validaciones al formulario editarProducto

--- src/components/views/producto/EditarProducto.jsx
+++ src/components/views/producto/EditarProducto.jsx
@@ -1,40 +1,72 @@
-import { Form, Button } from "react-bootstrap";
+import { Form, Button } from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
 
 const EditarProducto = () => {
-  
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const onSubmit = (producto) => {
+    console.log(producto);
+    //TODO: Hacer la función crearProducto en queries.js e incovarla aqui
+  };
+
   return (
     <section className="container mainSection">
       <h1 className="display-4 mt-5">Editar producto</h1>
       <hr />
-      <Form >
+      <Form onSubmit={handleSubmit(onSubmit)}>
         <Form.Group className="mb-3" controlId="formNombreProdcuto">
           <Form.Label>Producto*</Form.Label>
           <Form.Control
             type="text"
             placeholder="Ej: Cafe"
+            {...register('nombreProducto', {
+              required: 'El nombre del producto es un dato obligatorio.',
+            })}
           />
-         
+          <Form.Text className="text-danger my-2 py-3">
+            {errors.nombreProducto?.message}
+          </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formPrecio">
           <Form.Label>Precio*</Form.Label>
           <Form.Control
             type="number"
             placeholder="Ej: 50"
+            {...register('precio', {
+              required: 'El precio es un dato obligatorio.',
+            })}
           />
-       
+          <Form.Text className="text-danger my-2 py-3">
+            {errors.precio?.message}
+          </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formImagen">
           <Form.Label>Imagen URL*</Form.Label>
           <Form.Control
             type="text"
             placeholder="Ej: https://www.pexels.com/es-es/vans-en-blanco-y-negro-fuera-de-la-decoracion-para-colgar-en-la-pared-1230679/"
-      
+            {...register('imagen', {
+              required: 'La imagen es un dato obligatorio.',
+            })}
           />
-      
+          <Form.Text className="text-danger my-2 py-3">
+            {errors.imagen?.message}
+          </Form.Text>
         </Form.Group>
+
+        {/* TODO: Aqui va la Descripción a editar*/}
         <Form.Group className="mb-3" controlId="formPrecio">
           <Form.Label>Categoria*</Form.Label>
           <Form.Select
+            aria-label="Seleccione una categoría:"
+            {...register('categoriaProducto', {
+              required: 'Debe seleccionar una categoría',
+            })}
           >
             <option value="">Seleccione una opcion</option>
             <option value="bebida caliente">Bebida caliente</option>
@@ -42,7 +74,9 @@ const EditarProducto = () => {
             <option value="dulce">Dulce</option>
             <option value="salado">Salado</option>
           </Form.Select>
-       
+          <Form.Text className="text-danger my-2 py-3">
+            {errors.categoriaProducto?.message}
+          </Form.Text>
         </Form.Group>
         <Button variant="primary" type="submit">
           Guardar
