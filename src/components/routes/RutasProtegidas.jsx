f096c171cfccce97f98592d3b151a1c06922ff67
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-06-30 21:08:48 -0300

    se puso una altura igual a las imagenes del read y se redireccionó al index cuando no se encuentre el usuario en el storage

--- src/components/routes/RutasProtegidas.jsx
+++ src/components/routes/RutasProtegidas.jsx
@@ -3,11 +3,15 @@ import { Navigate } from 'react-router-dom';
 const RutasProtegidas = ({ children }) => {
   const usuarioLogueado = JSON.parse(sessionStorage.getItem('usuario')) || null;
 
-  if (usuarioLogueado.rol !== 'user') {
-    if (usuarioLogueado.rol !== 'admin') {
-      return <Navigate to={'/login'}></Navigate>;
+  if (usuarioLogueado !== null) {
+    if (usuarioLogueado.rol !== 'user') {
+      if (usuarioLogueado.rol !== 'admin') {
+        return <Navigate to={'/login'}></Navigate>;
+      } else {
+        return children;
+      }
     } else {
-      return children;
+      return <Navigate to={'/'}></Navigate>;
     }
   } else {
     return <Navigate to={'/'}></Navigate>;
