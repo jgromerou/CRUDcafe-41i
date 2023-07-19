6c3e833479453d3f2c9b761307619129c03b3299
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-07-19 20:16:10 -0300

    se practicó el maquetado Login del proyecto yum yum

--- src/components/views/Login.jsx
+++ src/components/views/Login.jsx
@@ -1,8 +1,10 @@
-import { Form, Button, Container, Card } from 'react-bootstrap';
+import { Form, Button, Container, Card, InputGroup } from 'react-bootstrap';
 import { login } from '../helpers/queries';
 import { useForm } from 'react-hook-form';
 import Swal from 'sweetalert2';
 import { useNavigate } from 'react-router-dom';
+import { ArrowRight } from 'react-bootstrap-icons';
+import './../../css/login.css';
 
 const Login = ({ setUsuarioLogueado }) => {
   const {
@@ -33,52 +35,79 @@ const Login = ({ setUsuarioLogueado }) => {
   };
 
   return (
-    <Container className="mainSection">
-      <Card className="my-5">
-        <Card.Header as="h5">Login</Card.Header>
+    <Container className="mainSection contenedorPrincipal">
+      <Card className="my-5 border-0 contenedor_login">
+        <Card.Title as="h2" className=" mt-3 text-center">
+          Iniciar Sesión
+        </Card.Title>
         <Card.Body>
           <Form onSubmit={handleSubmit(onSubmit)}>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Ingrese un email"
-                {...register('email', {
-                  required: 'El Email es un dato obligatorio.',
-                  pattern: {
-                    value:
-                      /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
-                    message:
-                      'El Email debe cumplir con el formato correo@correo.com',
-                  },
-                })}
-              />
+            <Form.Group className="mb-3" controlId="formEmail">
+              <InputGroup>
+                <InputGroup.Text id="iconoEmail" className="fondoIconoInput">
+                  <ArrowRight color="royalblue" size={10} />
+                </InputGroup.Text>
+                <Form.Control
+                  placeholder="Email"
+                  aria-label="Email"
+                  aria-describedby="iconoEmail"
+                  type="email"
+                  className="inputFormulario"
+                  {...register('email', {
+                    required: 'El Email es un dato obligatorio.',
+                    pattern: {
+                      value:
+                        /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
+                      message:
+                        'El Email debe cumplir con el formato correo@correo.com',
+                    },
+                  })}
+                />
+              </InputGroup>
               <Form.Text className="text-danger my-2 py-3">
                 {errors.email?.message}
               </Form.Text>
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Password"
-                {...register('password', {
-                  required: 'El Password es un dato obligatorio.',
-                  pattern: {
-                    value: /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/,
-                    message:
-                      'La contraseña debe tener al entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula.',
-                  },
-                })}
-              />
+
+            <Form.Group className="mb-3" controlId="formPassword">
+              <InputGroup>
+                <InputGroup.Text id="iconoPassword" className="fondoIconoInput">
+                  <ArrowRight color="royalblue" size={10} />
+                </InputGroup.Text>
+                <Form.Control
+                  placeholder="Contraseña"
+                  aria-label="Password"
+                  aria-describedby="iconoPassword"
+                  type="password"
+                  className="inputFormulario"
+                  {...register('password', {
+                    required: 'La contraseña es un dato obligatorio.',
+                    pattern: {
+                      value: /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/,
+                      message:
+                        'La contraseña debe tener al entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula.',
+                    },
+                  })}
+                />
+              </InputGroup>
               <Form.Text className="text-danger my-2 py-3">
                 {errors.password?.message}
               </Form.Text>
             </Form.Group>
-            <Button variant="primary" type="submit" className="mb-2">
+
+            <Button type="submit" className="mb-2 botonIngresar">
               Ingresar
             </Button>
           </Form>
+          <hr />
+          <div className="text-center">
+            <Card.Text as="h4" className="mt-3 subtituloCuenta">
+              ¿No tienes una cuenta?
+            </Card.Text>
+            <Card.Link href="#" className="linkRegistrate">
+              Registrate
+            </Card.Link>
+          </div>
         </Card.Body>
       </Card>
     </Container>
